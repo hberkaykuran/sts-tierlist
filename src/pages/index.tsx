44a341b308fb496c80f15635a3da4bd8bdb95503
hberkaykuran <berkaykuran54@gmail.com>
2022-11-19 19:56:38 +0300

    home page draft

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,12 @@ const Home: NextPage = () => {
         <title>StS Tierlist</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>TEST</div>
+      <div className="flex h-[calc(100vh-3rem)] w-[100vw] flex-col items-center justify-start">
+        <p className=" mt-24 text-7xl">Slay The Spire Tierlist</p>
+        <p className=" mt-12 text-2xl">
+          To contribute to the tierlist, go ahead and vote
+        </p>
+      </div>
     </>
   );
 };
